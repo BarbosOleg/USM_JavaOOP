5465ad90b2a054ce6f5c77135b23d9d3f26485ca
Oleg Barbos <62558059+KotikVKube@users.noreply.github.com>
2020-09-11 17:30:14 +0300

    commit...

---
+++ src/Arithmetic.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   Scanner input = new Scanner(System.in);
+	        int num1;
+	        int num2;
+	        int sum;
+	        int dif;
+	        int prod;
+	        double div;
+
+	        System.out.print("Introduceti primul numar: ");
+	        num1 = input.nextInt();
+
+	        System.out.print("Introduceti al doilea numar: ");
+	        num2 = input.nextInt();
+
+	        sum = num1 + num2;
+	        dif = num1 - num2;
+	        prod = num1 * num2;
+	        div = (float)num1 / num2;
+
+	        System.out.printf("Suma -> %d%n",sum);
+
+	        System.out.printf("Diferenta -> %d%n",dif);
+
+	        System.out.printf("Produsl -> %d%n",prod);
+
+	        System.out.printf("Impartiria -> %.2f%n",div);
+	}
+
+}
