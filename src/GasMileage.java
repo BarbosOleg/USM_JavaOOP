cb81139d97b60af01af60cc981563868615a876e
Oleg Barbos <62558059+KotikVKube@users.noreply.github.com>
2020-09-12 13:02:35 +0300

    commit...

--- src/GasMileage.java
+++ src/GasMileage.java
@@ -7,20 +7,27 @@ public class GasMileage {
 		Scanner input = new Scanner(System.in);
 		
 		int miles, gallons, trips = 0, tripsCount = 0;
+		double milePerGalon, totalMPG = 0;//mpg => miles driven gallons used
 		
 		while(trips != -1)
 		{
-			System.out.print("Enter trip nr. or -1 to EXIT :");
-			trips = input.nextInt();
 			
 			System.out.print("Get driven miles: ");
 			miles = input.nextInt();
-			System.out.print("Get used gallons");
+			System.out.print("Get used gallons: ");
 			gallons = input.nextInt();
 			
+			milePerGalon = (double)miles / gallons;
+			System.out.printf("Curent trip miles/gallon : %.3f%n", milePerGalon);
+			
+			totalMPG += milePerGalon;
+			
+			System.out.print("Enter trip nr. or -1 to EXIT : ");
+			trips = input.nextInt();
 			tripsCount++;
 		}
-		
+		System.out.printf("Total trips nr. : %d%n", tripsCount);
+		System.out.printf("Total miles/gallon : %.3f%n", totalMPG);
 		
 		
 	}
