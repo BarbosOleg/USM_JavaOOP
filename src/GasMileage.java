24e00ce26724d48d8d00b8e1d78d33931805f0c1
Oleg Barbos <62558059+KotikVKube@users.noreply.github.com>
2020-09-12 12:32:50 +0300

    commit...

--- src/GasMileage.java
+++ src/GasMileage.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
 
 public class GasMileage {
-
-	public GasMileage() {
-		// TODO Auto-generated constructor stub
-	}
-
+	@SuppressWarnings({ "unused", "resource" })
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		int miles, gallons, trips = 0, tripsCount = 0;
+		
+		while(trips != -1)
+		{
+			System.out.print("Enter trip nr. or -1 to EXIT :");
+			trips = input.nextInt();
+			
+			System.out.print("Get driven miles: ");
+			miles = input.nextInt();
+			System.out.print("Get used gallons");
+			gallons = input.nextInt();
+			
+			tripsCount++;
+		}
+		
+		
+		
 	}
 
 }
