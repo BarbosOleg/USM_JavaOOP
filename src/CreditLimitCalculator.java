6a9c250c7381b63bec52c45cfee54650e39b75af
Oleg Barbos <62558059+KotikVKube@users.noreply.github.com>
2020-09-12 13:38:35 +0300

    commit...

---
+++ src/CreditLimitCalculator.java
@@ -0,0 +1,15 @@
+/**
+ * @author BarbosOleg
+ *
+ */
+public class CreditLimitCalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
