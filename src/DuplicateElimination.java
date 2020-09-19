60bb57adab0f58234542535b0596bd7ae0f7b572
Oleg Barbos <62558059+KotikVKube@users.noreply.github.com>
2020-09-19 13:16:52 +0300

    commit

---
+++ src/DuplicateElimination.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DuplicateElimination {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int counter = 5;
+		int tmp;
+		
+		while(counter != 0) {
+			System.out.print("Get a number: ");
+			tmp = input.nextInt();
+			if (tmp >= 10 && tmp <= 100) {
+				
+			}
+		}
+	}
+
+}
