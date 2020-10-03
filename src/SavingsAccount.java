ec7750ec19c2ad67797b0e94f4686b82e6d51e5d
Oleg Barbos <62558059+KotikVKube@users.noreply.github.com>
2020-10-03 10:14:46 +0300

    commit

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -3,18 +3,29 @@ public class SavingsAccount {
 	private static double InterestRate;
 	private double SavingBalance;
 	
+	public SavingsAccount(double interestRate, double savingBalance){
+		modifyInterestRate(interestRate);
+		setSavingBalance(savingBalance);
+	}
+	
 	public static double getInterestRate() {
 		return InterestRate;
 	}
 	public static void modifyInterestRate(double interestRate) {
-		InterestRate = interestRate;
+		if (interestRate > 0.0) {
+			InterestRate = interestRate;
+			
+		}else InterestRate = 0.0;
 	}
 	
 	public double getSavingBalance() {
 		return SavingBalance;
 	}
 	public void setSavingBalance(double savingBalance) {
-		SavingBalance = savingBalance;
+		if (savingBalance > 0.0) {
+			SavingBalance = savingBalance;
+			
+		}else SavingBalance = 0.0;
 	}
 	public double calculateMonthlyInterest() {
 		double tmpBalance;
--- src/TestSavingsAccount.java
+++ src/TestSavingsAccount.java
@@ -1,9 +1,17 @@
-
 public class TestSavingsAccount {
 
 	public static void main(String[] args) {
-		
 
+		SavingsAccount saving1 = new SavingsAccount(4, 2000);
+		SavingsAccount saving2 = new SavingsAccount(4, 3000);
+		
+		System.out.printf("New balance for saving1: $%.2f%n", saving1.calculateMonthlyInterest());
+		System.out.printf("New balance for saving2: $%.2f%n", saving2.calculateMonthlyInterest());
+		
+		SavingsAccount.modifyInterestRate(5.0);
+		
+		System.out.printf("New balance for saving1 with new interest rate: $%.2f%n", saving1.calculateMonthlyInterest());
+		System.out.printf("New balance for saving2 with new interest rate: $%.2f%n", saving2.calculateMonthlyInterest());
 	}
 
 }
