79aa932afdfa619bdc2dd1ba4f52d445dc082e37
BarbosOleg <lolipo.celio@gmail.com>
2020-10-10 12:59:53 +0300

    commit

---
+++ src/Employee.java
@@ -0,0 +1,37 @@
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+
+    public Employee(String firstName, String lastName, String socialSecurityNumber)
+    {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setSocialSecurityNumber(socialSecurityNumber);
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+    public String getSocialSecurityNumber()
+    {
+        return socialSecurityNumber;
+    }
+    public void setSocialSecurityNumber(String securityNumber)
+    {
+        socialSecurityNumber = securityNumber;
+    }
+}
